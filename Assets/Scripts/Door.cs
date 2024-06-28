e15b51fc19841bf94948c53363282e9b2cc394cf
Frank-Hudson <frank.hudson.v0@gmail.com>
2024-06-28 12:15:28 +0100

    Add next scene and position serialised fields to Door script

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,17 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+using OneOf;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other) {
-        // Show "press `e` to <interact>"
+    [SerializeField] private Scene? _nextScene;
+    [SerializeField] private Vector3? _nextPosition;
+
+    private OneOf<Scene, Vector3> _next;
+
+    private void Awake()
+    {
+        if (_nextScene.HasValue)
+        {
+            _next = _nextScene.Value;
+        }
+        else if (_nextPosition.HasValue)
+        {
+            _next = _nextPosition.Value;
+        }
     }
 
-    private void OnTriggerStay(Collider other) {
-        if (Input.GetKey(KeyCode.E))
+    private void OnTriggerEnter(Collider other)
+    {
+        // Show prompt "Press `e` to <interact>"
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(Actions.INTERACT_KEY))
         {
-            
+
         }
     }
 }
